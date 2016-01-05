5a97d09d0233a24a61662cb1bc8dc607fc96fa44
Claudio Giordano <claudio.giordano@autistici.org>
2016-01-05 22:41:17 +0100

    Refactor FileReader internal properties

--- src/FileReader.php
+++ src/FileReader.php
@@ -8,10 +8,10 @@ namespace clagiordano\weblibs\localization;
  */
 class FileReader implements Reader
 {
-    private $position;
+    private $filePosition;
     private $fileHandle;
-    private $length;
-    private $error;
+    private $fileLength;
+    private $errorCode;
 
     /**
      * FileReader constructor.
@@ -21,16 +21,16 @@ class FileReader implements Reader
     public function __construct($filename)
     {
         if (file_exists($filename)) {
-            $this->length = filesize($filename);
-            $this->position = 0;
+            $this->fileLength = filesize($filename);
+            $this->filePosition = 0;
             $this->fileHandle = fopen($filename, 'rb');
 
             if (!$this->fileHandle) {
-                $this->error = 3; // Cannot read file, probably permissions
+                $this->errorCode = 3; // Cannot read file, probably permissions
                 return false;
             }
         } else {
-            $this->error = 2; // File doesn't exist
+            $this->errorCode = 2; // File doesn't exist
             return false;
         }
     }
@@ -44,7 +44,7 @@ class FileReader implements Reader
         $data = "";
 
         if ($bytes) {
-            fseek($this->fileHandle, $this->position);
+            fseek($this->fileHandle, $this->filePosition);
 
             // PHP 5.1.1 does not read more than 8192 bytes in one fread()
             // the discussions at PHP Bugs suggest it's the intended behaviour
@@ -53,7 +53,7 @@ class FileReader implements Reader
                 $data .= $chunk;
                 $bytes -= strlen($chunk);
             }
-            $this->position = ftell($this->fileHandle);
+            $this->filePosition = ftell($this->fileHandle);
         }
 
         return $data;
@@ -66,9 +66,9 @@ class FileReader implements Reader
     public function seekTo($position)
     {
         fseek($this->fileHandle, $position);
-        $this->position = ftell($this->fileHandle);
+        $this->filePosition = ftell($this->fileHandle);
 
-        return $this->position;
+        return $this->filePosition;
     }
 
     /**
@@ -76,7 +76,7 @@ class FileReader implements Reader
      */
     public function currentPos()
     {
-        return $this->position;
+        return $this->filePosition;
     }
 
     /**
@@ -84,7 +84,7 @@ class FileReader implements Reader
      */
     public function length()
     {
-        return $this->length;
+        return $this->fileLength;
     }
 
     /**
